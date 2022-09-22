7727eac3bb314e42ad31ef12b32ef7951c64d61a
A <aryaki.play@gmail.com>
2022-09-22 13:08:54 +0200

    UseMe uWu

--- 20.9.2022.py
+++ 20.9.2022.py
@@ -1,7 +1,52 @@
+import math
 
-x = 5
+# quadratic formula
+
+a = 2
+b = 4
+c = 2
 
 
+def q_formula(a, b, c):
+
+    x1 = (-b + math.sqrt((b ** 2) - (4 * a * c)) / 2 * a)
+    x2 = (-b - math.sqrt((b ** 2) - (4 * a * c)) / 2 * a)
+
+    return x1, x2
+
+
+try:
+
+    x1, x2 = q_formula(a, b, c)
+
+    if x1 != x2:
+        print("2 korene")
+
+    else:
+        print("1 koren")
+
+except:
+    print("diskriminant je zaporny ")
+
+
+# new problem
+# triangle
+
+a = 3
+b = 2
+c = 4
+
+if (a + b) > c and (a + c) > b and (b + c) > a:
+    print("exists")
+
+if a == b == c:
+    print("rovnostranny")
+
+elif a == b or a == c or b == c:
+    print("rovnoramenny")
+
+else:
+    print("△")
 
 
 
---
+++ 22.9.2022.py
@@ -0,0 +1,24 @@
+mean = 2
+maximum = 3
+
+if mean <= 1.5 and maximum <= 2:
+    print("vyznamenanie")
+
+elif mean <= 2 and maximum <= 3:
+    print("velmi dobre")
+
+elif maximum == 4:
+    print("prospel")
+
+else:
+    print("neprospel")
+
+
+
+
+
+
+
+
+
+
