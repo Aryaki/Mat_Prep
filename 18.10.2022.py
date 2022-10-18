509d63a77748e949c32738868e3fffd23302a87a
A <aryaki.play@gmail.com>
2022-10-18 09:19:34 +0200

    UseMe uWu

--- 18.10.2022.py
+++ 18.10.2022.py
@@ -21,9 +21,19 @@ for i in range(10):
         points += 1
 
     else:
-        print("You failed")
+        print(f"You failed, the answer was {sol}")
 
 end = time.time()
 
 print(f"Your score is {points}/30 which is {round((points / 30) * 100)}%")
 print(f"Your time was {end - start}")
+
+################################
+
+n = 50000
+throws = 6*[0]
+for i in range(n):
+    x = rn.randint(1, 6)
+    throws[x - 1] += 1
+print(f"The most common one is {throws.index(max(throws))} with {max(throws)} throws")
+
