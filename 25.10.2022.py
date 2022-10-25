b4b498d5960613535c5672fe1553edddaa463635
A <aryaki.play@gmail.com>
2022-10-25 08:21:47 +0200

    UseMe uWu

--- 25.10.2022.py
+++ 25.10.2022.py
@@ -1,5 +1,5 @@
 
-salary = 1843
+salary = 8000
 
 nums = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 output = []
@@ -7,4 +7,4 @@ output = []
 for i in range(len(nums)):
     if salary // nums[i] >= 0:
         output.append(f"{salary // nums[i]} * {nums[i]}")
-        salary = (salary // nums[i]) * nums[i]
+        salary -= (salary // nums[i]) * nums[i]
