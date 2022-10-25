82e2112169595d60ba390c8cd1281cfb51dd8694
A <aryaki.play@gmail.com>
2022-10-25 08:55:28 +0200

    UseMe uWu

--- 25.10.2022.py
+++ 25.10.2022.py
@@ -1,10 +1,15 @@
-
-salary = 8000
-
-nums = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-output = []
-
+salary, nums = 8000, [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 for i in range(len(nums)):
     if salary // nums[i] >= 0:
-        output.append(f"{salary // nums[i]} * {nums[i]}")
+        print(f"{salary // nums[i]} * {nums[i]}")
         salary -= (salary // nums[i]) * nums[i]
+#############################################################
+date = "12.13.2012"
+day, month, year = date.split(".")
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if int(month) > 12 or int(day) > months[int(month)]:print("Wrong")
+else:
+    days = 0
+    days += months[int(month)] - int(day)
+    for i in range(int(month), len(months)):days += months[i]
+#############################################################
